6117746c92e674ede8fa1ce435f5ef3fd17d1fbc
Muntasir Biojid <muntasirbiojid@Muntasirs-MacBook-Pro.local>
2019-04-19 10:37:27 -0700

    imported HashRouter instead of BrowserRouter to deploy the app to github via gh-pages

---
+++ build/precache-manifest.5046898f9d8ac877dc21db99b758a18f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads-starter/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "83f7d75ca12013b90cc6",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime~main.83f7d75c.js"
+  },
+  {
+    "revision": "04cf03e0c2a5652177e8",
+    "url": "/reactnd-project-myreads-starter/static/js/main.04cf03e0.chunk.js"
+  },
+  {
+    "revision": "bb61cdf229dfaacb9369",
+    "url": "/reactnd-project-myreads-starter/static/js/1.bb61cdf2.chunk.js"
+  },
+  {
+    "revision": "04cf03e0c2a5652177e8",
+    "url": "/reactnd-project-myreads-starter/static/css/main.12e81f67.chunk.css"
+  },
+  {
+    "revision": "27ce2db2de6e965c7a90ff1757a4988a",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  }
+];
